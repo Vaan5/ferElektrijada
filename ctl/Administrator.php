281ec34a5406ff55f0898629131412ac35fa1514
Vaan5 <denis.causevic5@gmail.com>
2014-03-27 09:58:22 +0100

    Dodana akcija u Administrator za dodavanje elektrijade + napravljen  formModel za to

--- ctl/Administrator.php
+++ ctl/Administrator.php
@@ -6,12 +6,16 @@ use app\controller\Controller;
 class Administrator implements Controller {
     
     private $errorMessage;
-
-    public function addOzsn() {
+    
+    private function checkRole() {
         // if you don't have the right permissions to do this, then get out of here
         if (!\model\DBOsoba::isLoggedIn() || \model\DBOsoba::getUserRole() !== 'A') {
             preusmjeri(\route\Route::get('d1')->generate() . "?msg=accessDenied");
         }
+    }
+
+    public function addOzsn() {
+        $this->checkRole();
         
         // if you want to add someone
         if (!postEmpty()) {
@@ -45,8 +49,6 @@ class Administrator implements Controller {
                         preusmjeri(\route\Route::get('d1')->generate() . "?msg=ozsnAddedSucc");
                 } catch (\PDOException $e) {
                     $this->errorMessage = "Pogreška prilikom dodavanja u bazu! Provjerite da li korisnik s takvim podacima već ne postoji!";
-                }catch (Exception $e) {
-                    $this->errorMessage = "Pogreška prilikom dodavanja u bazu! Provjerite da li korisnik s takvim podacima već ne postoji!";
                 }
             }
         }
@@ -58,5 +60,49 @@ class Administrator implements Controller {
             "title" => "Dodavanje članova odbora"
         ));
         
+    }
+    
+    public function addElektrijada() {
+        $this->checkRole();
+        
+        if(!postEmpty()) {
+            $validacija = new \model\ElektrijadaFormModel(array(
+                                                'mjestoOdrzavanja' => post('mjestoOdrzavanja'),
+                                            'datumPocetka' => post('datumPocetka'),
+                                            'datumKraja' => post('datumKraja'), 
+                                            'ukupniRezultat' => post('ukupniRezultat'),
+                                            'drzava' => post('drzava')
+                                            ));
+            $pov = $validacija->validate();
+            if($pov !== true) {
+                $this->errorMessage = $validacija->decypherErrors($pov);
+            } else {
+                // everything's ok i add the new Elektrijada data
+                $elektrijada = new \model\DBElektrijada();
+                
+//                // dodati u DBElektrijada
+//                public function addNewElektrijada($mjestoOdrzavanja, $datumPocetka, $datumKraja, $ukupniRezultat, $drzava) {
+//                    $this->idElektrijade = null;
+//                    $atributi = $this->getColumns();
+//                    foreach($atributi as $a) {
+//                        $this->{$a} = ${$a};
+//                    }
+//                    $this->save();
+//                }
+                
+                try {
+                    $elektrijada->addNewElektrijada(post('mjestoOdrzavanja'), post('datumPocetka'), 
+                            post('datumKraja'), post('ukupniRezultat', NULL), post('drzava'));
+                } catch (PDOException $e) {
+                    $this->errorMessage = "Pogreška prilikom dodavanja u bazu! Provjerite da li elektrijada s takvim podacima već ne postoji!";
+                }
+                
+            }
+        }
+        
+        echo new \view\Main(array(
+            "body" => "Ovdje dolazi Randyjev pogled",
+            "title" => "Dodavanje Elektrijade"
+        ));
     }
 }
---
+++ model/ElektrijadaFormModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace model;
+use app\model\AbstractFormModel;
+
+class ElektrijadaFormModel extends AbstractFormModel {
+    
+    protected function rules() {
+        return array('mjestoOdrzavanja' => array('required', 'name'),
+            'datumPocetka' => array('required', 'date'),
+            'datumKraja' => array('required', 'date'), 
+            'ukupniRezultat' => array('numbers'),
+            'drzava' => array('required', 'name'));
+    }
+    
+    public function decypherErrors($pov) {
+        if(count($pov)) {
+            foreach ($pov as $k => $v) {
+                switch ($k) {
+                    case 'mjestoOdrzavanja':
+                        return $v === 'required' ? 'Morate unijeti mjesto održavanja' : 'Pogrešno mjesto održavanja';
+                    case 'datumPocetka':
+                        return $v === 'required' ? 'Morate unijeti datum početka Elektrijade' : 'Pogrešan datum početka Elektrijade';
+                    case 'datumKraja':
+                        return $v === 'required' ? 'Morate unijeti datum završetka Elektrijade' : 'Pogrešan datum završetka Elektrijade';
+                    case 'ukupniRezultat':
+                        return 'Neispravan rezultat! Mora biti broj!';
+                    case 'drzava':
+                        return $v === 'required' ? 'Morate unijeti naziv države' : 'Pogrešan naziv države';
+                    default:
+                        break;
+                }
+            }
+        }
+        return null;
+    }
+}
