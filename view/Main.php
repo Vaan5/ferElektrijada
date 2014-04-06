33f6be1954d71521fc6358c3b119b058245cbf0c
JoeKurta <martin.kurtoic@gmail.com>
2014-04-06 14:39:32 +0200

    popravljena greška do koje je dolazilo ukoliko nije bio ulogiran korisnik

--- view/Main.php
+++ view/Main.php
@@ -79,7 +79,7 @@ class Main extends AbstractView {
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">    
         <ul class="nav navbar-nav">
-        <?php if( $_SESSION ['vrsta']==='A') echo 
+        <?php if(\model\DBOsoba::isLoggedIn() && $_SESSION ['vrsta']==='A') echo 
             "<li><a href=\"" . \route\Route::get('d3')->generate(array(
                     "controller" => 'administrator',
                     "action" => 'addOzsn'
