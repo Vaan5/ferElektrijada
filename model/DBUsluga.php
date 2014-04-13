b92c7d0efdfef204cb51e346e7ef77228042b656
Scepo93 <stipe46@gmail.com>
2014-04-13 11:08:24 +0200

    DBUsluga-pravi file

--- model/DBUsluga.php
+++ model/DBUsluga.php
@@ -17,8 +17,66 @@ class DBUsluga extends AbstractDBModel {
         return array('nazivUsluge');
     }
     
-    public function getAll() {
+    public function getAllUsluga() {
 	return $this->select()->fetchAll();
+    }
+	
+	/**
+     * Modifies row in the database
+     * 
+     * @param mixed $idUsluge
+     * @param mixed $nazivUsluge
+     */
+    public function modifyRow($idUsluge, $nazivUsluge) {
+        try {
+            $this->load($idUsluge);
+            $this->nazivUsluge = $nazivUsluge;
+            $this->save();
+        } catch (\app\model\NotFoundException $e) {
+            $e = new \PDOException();
+            $e->errorInfo[0] = '02000';
+            $e->errorInfo[1] = 1604;
+            $e->errorInfo[2] = "Zapis ne postoji!";
+            throw $e;
+        } catch (\PDOException $e) {
+            throw $e;
+        }
+    }
+	
+	/**
+     * Deletes a row from the table 
+     * 
+     * @param mixed $idUsluge
+     * @throws \model\NotFoundException
+     */
+    public function deleteRow($idUsluge) {
+        try {
+            $this->load($idUsluge);
+            $this->delete();
+        } catch (\app\model\NotFoundException $e) {
+            $e = new \PDOException();
+            $e->errorInfo[0] = '02000';
+            $e->errorInfo[1] = 1604;
+            $e->errorInfo[2] = "Zapis ne postoji!";
+            throw $e;
+        } catch (\PDOException $e) {
+            throw $e;
+        }
+    }
+	
+	 /**
+     * Adds row to the database
+     * 
+     * @param mixed $nazivUsluge
+     * @throws \model\PDOException
+     */
+    public function addRow($nazivUsluge) {
+        try {
+            $this->nazivUsluge = $nazivUsluge;
+            $this->save();
+        } catch (\PDOException $e) {
+            throw $e;
+        }
     }
 }
 
