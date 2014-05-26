2c7cb398fb9ef25974fbdf9b6a647d45a7800574
Vaan5 <denis.causevic5@gmail.com>
2014-05-26 23:58:35 +0200

    Voditelj, ne vidi glupost kod moj tim

--- ctl/Sudionik.php
+++ ctl/Sudionik.php
@@ -391,6 +391,11 @@ class Sudionik implements Controller {
 				$voditelji = $imaatribut->getVoditelji(post("idPodrucja"), $idElektrijade);
 			} else {
 				$podrucja = $sudjelovanje->getContestantAreas(session("auth"), $idElektrijade);
+				if (session("vrsta") === 'SV' && count($podrucja) === 0)
+					preusmjeri (\route\Route::get('d3')->generate (array(
+						"controller" => "voditelj",
+						"action" => "displayPodrucja"
+					)));
 			}
 		} catch (\app\model\NotFoundException $e) {
 			$this->createMessage("Nepostojeći identifikator!", "d3", "sudionik", "displayMyTeam");
